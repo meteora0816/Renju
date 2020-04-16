9dfc37daead286fbfe4dd11a3a6ceb7176d0a552
li <lxx610103@163.com>
2020-04-16 21:04:51 +0800

    悔棋与复盘

--- renju/makemove.cpp
+++ renju/makemove.cpp
@@ -6,6 +6,7 @@ bool makeMove(int x, int y, int color)
 {
 	//执行落子操作（chessboard[i][j]=1 & chessboard[i][j]=2）
 	chessBoard[x][y] = color;
+	moveStep.push_back(make_pair(x,y));
 	piecesCount++;
 	printf("%d %d %d\n", x, y, color);
 	int delta = evaluate(x, y);
@@ -18,5 +19,16 @@ bool makeMove(int x, int y, int color)
 bool unMakeMove()
 {
 	//撤销落子操作（chessboard[i][j]=0）
-	return true;
+	pair<int, int> p1, p2;
+	p1 = moveStep.back();
+	moveStep.pop_back();
+	p2 = moveStep.back();
+	moveStep.pop_back();
+	chessBoard[p1.first][p1.second] = 0;
+	chessBoard[p2.first][p2.second] = 0;
+	piecesCount -= 2;
+	if (chessBoard[p1.first][p1.second] == 0 && chessBoard[p2.first][p2.second] == 0)
+		return true;
+	else
+		return false;
 }
